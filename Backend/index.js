448fcd54a03e447f4ab2e9d242eeaacca6f80cc7
Simran <simranmalhi1612@gmail.com>
2026-02-13 23:54:17 +0530

    Fix merge conflict markers in index.js

--- Backend/index.js
+++ Backend/index.js
@@ -5,50 +5,43 @@ import cors from "cors"
 import chatbotRoutes from './routes/chatbot.routes.js';
 import authRoutes from './routes/auth.routes.js'
 import chatHistoryRoutes from './routes/chatHistory.routes.js';
+
+
 const app = express()
+
 dotenv.config()
+
 const port = process.env.PORT || 3000
 //middlewares
 app.use(cors({
     origin: function (origin, callback) {
         // Allow requests with no origin (like mobile apps or curl requests)
         if (!origin) return callback(null, true);
-<<<<<<< HEAD
-=======
 
->>>>>>> 6dffb79 (adding frontend)
         // Allow localhost (development)
         if (origin.includes("localhost")) {
             return callback(null, true);
         }
-<<<<<<< HEAD
-=======
 
->>>>>>> 6dffb79 (adding frontend)
         // Allow all Vercel deployments (preview + production)
         if (origin.includes("vercel.app")) {
             return callback(null, true);
         }
-<<<<<<< HEAD
-=======
 
->>>>>>> 6dffb79 (adding frontend)
         // Block everything else
         return callback(new Error("Not allowed by CORS"));
     },
     credentials: true
 }));
-<<<<<<< HEAD
-app.use(express.json());
-=======
 
 app.use(express.json());
 
->>>>>>> 6dffb79 (adding frontend)
 // Routes
 app.use('/api/auth', authRoutes); //Register && login
 app.use("/api/chats", chatHistoryRoutes);  //chat history
 app.use('/bot/v1/', chatbotRoutes); // // Chatbot Messages
+
+
 //Database Connection code
 mongoose.connect(process.env.MONGO_URI)
     .then(() => {
@@ -56,9 +49,13 @@ mongoose.connect(process.env.MONGO_URI)
     }).catch((error) => {
         console.log("Error connecting to MongoDB:", error)
     })
+
+
+
 app.get('/', (req, res) => {
     res.end("Hello World!")
 })
+
 app.listen(port, () => {
     console.log(`Server is running on port ${port}`)
 })
